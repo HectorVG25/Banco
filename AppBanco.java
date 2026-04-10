80cdc397f98b0a3e71971d4b9e0aec55f6034d32
Hector Villamediana <hector.villamediana@educa.madrid.org>
2026-04-10 09:40:59 +0200

    Commit Inicial

---
+++ AppBanco.java
@@ -0,0 +1,7 @@
+public class AppBanco {
+    public static void main(String[] args) {
+        Cliente c1 = new Cliente("4569650P");
+        System.err.println(c1);
+    }
+    
+}
---
+++ Cuenta.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+public class Cuenta {
+    private int numeroCuenta;
+    private double saldo;
+    private Cliente titular;
+    private ArrayList<Movimiento> movimientos;
+
+    
+    public Cuenta(int numeroCuenta, double saldo, Cliente titular) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+        this.titular = titular;
+    }
+
+
+    public int getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+
+    public void setNumeroCuenta(int numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+
+    public Cliente getTitular() {
+        return titular;
+    }
+
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+
+    
+
+
+
+    
+}
